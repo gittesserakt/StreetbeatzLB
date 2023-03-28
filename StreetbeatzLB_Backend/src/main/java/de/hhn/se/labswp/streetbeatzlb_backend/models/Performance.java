a8dd79f2c1f5281bc20aeea1539a819097904b57
Nicolas Holl <nholl@stud.hs-heilbronn.de>
2023-03-28 20:15:44 +0200

    corrected Performance Entity

--- StreetbeatzLB_Backend/src/main/java/de/hhn/se/labswp/streetbeatzlb_backend/models/Performance.java
+++ StreetbeatzLB_Backend/src/main/java/de/hhn/se/labswp/streetbeatzlb_backend/models/Performance.java
@@ -3,43 +3,59 @@ package de.hhn.se.labswp.streetbeatzlb_backend.models;
 import jakarta.persistence.*;
 
 import java.sql.Time;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 public class Performance {
 
   @Id
-  @Column(name="PerormanceNo")
   @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+  private Long performance_id;
+  private String date_time;
+  private String created_by;
+  private Long group_id;
+  private Long stage_id;
 
-  @Column(name="StageStageNo")
-  private char stageNr;
-
-  public char getStageNr() {
-    return stageNr;
+  public Long getPerformance_id() {
+    return performance_id;
   }
 
-  public void setStageNr(char stageNr) {
-    this.stageNr = stageNr;
+  public void setPerformance_id(Long performance_id) {
+    this.performance_id = performance_id;
   }
 
-  public Time getTime() {
-    return time;
+  public LocalDateTime getDate_time() {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    return LocalDateTime.parse(date_time,formatter);
   }
 
-  public void setTime(Time time) {
-    this.time = time;
+  public void setDate_time(LocalDateTime date_time) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    this.date_time = date_time.format(formatter);
   }
 
-  @Column(name="Time")
-  private Time time;
-
-  public void setId(Long id) {
-    this.id = id;
+  public String getCreated_by() {
+    return created_by;
   }
 
-  @Id
-  public Long getId() {
-    return id;
+  public void setCreated_by(String created_by) {
+    this.created_by = created_by;
+  }
+
+  public Long getGroup_id() {
+    return group_id;
+  }
+
+  public void setGroup_id(Long group_id) {
+    this.group_id = group_id;
+  }
+
+  public Long getStage_id() {
+    return stage_id;
+  }
+
+  public void setStage_id(Long stage_id) {
+    this.stage_id = stage_id;
   }
 }
