5a0db3240e66ff45c66570d5353d9b6778cd5038
JxWorker <micheljouaux@gmx.de>
2023-04-05 11:24:54 +0200

    Fix: New Navbar with AngularMaterial

--- StreetbeatzLB_Frontend/src/app/shared/shared.module.ts
+++ StreetbeatzLB_Frontend/src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import { COMPONENTS } from './components';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 
 @NgModule({
@@ -9,6 +11,8 @@ import { COMPONENTS } from './components';
   ],
   imports: [
     CommonModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   exports: [
     ...COMPONENTS
