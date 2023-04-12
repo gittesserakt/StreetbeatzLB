322ed64844f63cf4c381aaa9b34e3825beb7bdcf
Moutassem Mousa <mmousa@stud.hs-heilbronn.de>
2023-04-12 15:40:53 +0200

    Changed secondary sorting for performances from stage to artist names

--- StreetbeatzLB_Backend/src/main/java/de/hhn/se/labswp/streetbeatzlb_backend/controllers/prod/PerformanceController.java
+++ StreetbeatzLB_Backend/src/main/java/de/hhn/se/labswp/streetbeatzlb_backend/controllers/prod/PerformanceController.java
@@ -1,15 +1,21 @@
 package de.hhn.se.labswp.streetbeatzlb_backend.controllers.prod;
 
+import de.hhn.se.labswp.streetbeatzlb_backend.models.Artist;
+import de.hhn.se.labswp.streetbeatzlb_backend.models.ArtistRepository;
 import de.hhn.se.labswp.streetbeatzlb_backend.models.Performance;
 import de.hhn.se.labswp.streetbeatzlb_backend.models.PerformanceFilter;
 import de.hhn.se.labswp.streetbeatzlb_backend.models.PerformanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.Map;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Controller
 @RequestMapping(path="/api/performances")
@@ -17,6 +23,9 @@ import java.util.List;
 public class PerformanceController {
   @Autowired
   private PerformanceRepository performanceRepository;
+
+  @Autowired
+  private ArtistRepository artistRepository;
 
   @GetMapping(path="/all")
   public @ResponseBody Iterable<Performance> getAllPerformances() {
@@ -35,14 +44,14 @@ public class PerformanceController {
   }
 
   private Iterable<Performance> sortPerformances(Iterable<Performance> performances){
+    List<Artist> artists = (List<Artist>) artistRepository.findAll();
 
-    List<Performance> sortedPerformances = (List<Performance>) performances;
+    Map<Long, String> artistNameMap = artists.stream()
+            .collect(Collectors.toMap(Artist::getArtist_id, Artist::getName));
 
-    Comparator<Performance> startTimeComparator = Comparator.comparing(Performance::getStart_time);
-    Comparator<Performance> stageComparator = Comparator.comparing(Performance::getStage_id);
-
-    sortedPerformances.sort(startTimeComparator.thenComparing(stageComparator));
-
-    return sortedPerformances;
+    return StreamSupport.stream(performances.spliterator(), false)
+            .sorted(Comparator.comparing(Performance::getStart_time)
+                    .thenComparing(p -> artistNameMap.getOrDefault(p.getArtist_id(), "")))
+            .collect(Collectors.toList());
   }
 }
