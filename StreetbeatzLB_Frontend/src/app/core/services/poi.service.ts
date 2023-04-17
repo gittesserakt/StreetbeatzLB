24004e08b3ec9010a6dd81cc1f15b718bd996a1a
Nicolas Holl <nholl@stud.hs-heilbronn.de>
2023-04-17 12:43:56 +0200

    added Api in frontend

--- StreetbeatzLB_Frontend/src/app/core/services/poi.service.ts
+++ StreetbeatzLB_Frontend/src/app/core/services/poi.service.ts
@@ -2,18 +2,19 @@ import {ExternalApiService} from "./external-api.service";
 import {Injectable} from "@angular/core";
 import {environment as env } from '../../../environments/environment';
 import {mergeMap, Observable, of } from "rxjs";
-import {ApiResponseModel, Performance, RequestConfigModel} from "../models";
+import {ApiResponseModel, RequestConfigModel} from "../models";
+import {Poi} from "../models/poi.model";
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class PerformanceService {
-  constructor(public externalApiService: ExternalApiService) {}
-
-  getAllPerformances = (): Observable<ApiResponseModel> => {
+export class PoiService {
+  constructor(public externalApiService: ExternalApiService) {
+  }
+  getAllPois = ():Observable<ApiResponseModel> => {
     const config: RequestConfigModel = {
-      url: `${env.api.serverUrl}/performances/all`,
+      url: `${env.api.serverUrl}/pois/all`,
       method: 'GET',
       headers: {
         'content-type': 'application/json',
@@ -24,9 +25,9 @@ export class PerformanceService {
       mergeMap((response) => {
         const { data, error } = response;
 
-        return of({
-          data: data ? (data as Performance[]) : null,
-          error,
+        return of ({
+          data: data ? (data as Poi[]) : null,
+          error
         });
       })
     );
