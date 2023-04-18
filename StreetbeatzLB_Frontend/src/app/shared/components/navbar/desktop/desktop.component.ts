cc5e3ecc88c090b6c963c3e1ccb3994ff3c5f593
Hannes Jetter <hannes.jetter@googlemail.com>
2023-04-19 01:44:47 +0200

    created variables for navbar height that get applied to every navbar

--- StreetbeatzLB_Frontend/src/app/shared/components/navbar/desktop/desktop.component.ts
+++ StreetbeatzLB_Frontend/src/app/shared/components/navbar/desktop/desktop.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-desktop',
   templateUrl: './desktop.component.html',
   styleUrls: ['./desktop.component.scss']
 })
-export class DesktopComponent {
+export class DesktopComponent implements OnInit{
+  @Input() navbarHeight: number = 64;
 
+  ngOnInit(): void {
+    document.documentElement.style.setProperty('--navbar-height', this.navbarHeight + 'px');
+  }
 }
--- StreetbeatzLB_Frontend/src/app/shared/components/navbar/mobile/mobile.component.ts
+++ StreetbeatzLB_Frontend/src/app/shared/components/navbar/mobile/mobile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {MatIconRegistry} from "@angular/material/icon";
 import {DomSanitizer} from "@angular/platform-browser";
 
@@ -7,8 +7,14 @@ import {DomSanitizer} from "@angular/platform-browser";
   templateUrl: './mobile.component.html',
   styleUrls: ['./mobile.component.scss']
 })
-export class MobileComponent {
+export class MobileComponent implements OnInit {
+  @Input() navbarHeight: number = 64;
+
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     iconRegistry.addSvgIcon('streetbeatz', sanitizer.bypassSecurityTrustResourceUrl('./assets/streetbeatzLogo/logo.svg'));
+  }
+
+  ngOnInit(): void {
+    document.documentElement.style.setProperty('--navbar-height', this.navbarHeight + 'px');
   }
 }
