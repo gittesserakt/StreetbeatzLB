f6808ab85af10fc02900f8b6128e78fa187dd4ca
Hannes Jetter <hannes.jetter@googlemail.com>
2023-05-03 23:14:54 +0200

    added ssl to backend

--- docker/startTomcat.sh
+++ docker/startTomcat.sh
@@ -6,4 +6,5 @@ mv webapps.dist webapps
 
 # start tomcat
 cp /var/www/streetbeatzlb/StreetbeatzLB_Backend.war ./webapps/streetbeatzlb.war
+/bin/keytool -import -alias hjetter -file /data/hjetter.p12 -keystore /usr/local/tomcat/conf/keystore.p12 -storepass streetbeatzlb -noprompt
 /usr/local/tomcat/bin/catalina.sh run
