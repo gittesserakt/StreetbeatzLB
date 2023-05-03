48f9c5f9d8a2c4f1dd5d6bb2702155e200faca89
Hannes Jetter <hannes.jetter@googlemail.com>
2023-05-04 00:47:07 +0200

    added keystore to backend

--- docker/startTomcat.sh
+++ docker/startTomcat.sh
@@ -6,5 +6,4 @@ mv webapps.dist webapps
 
 # start tomcat
 cp /var/www/streetbeatzlb/StreetbeatzLB_Backend.war ./webapps/streetbeatzlb.war
-keytool -import -alias hjetter -file /data/hjetter.p12 -keystore /usr/local/tomcat/conf/keystore.p12 -storepass streetbeatzlb -noprompt
 /usr/local/tomcat/bin/catalina.sh run
