e1ae100cd046650e935b6471486b1179ed1e25e4
Nicolas Holl <nholl@stud.hs-heilbronn.de>
2023-05-07 22:12:01 +0200

    added Cookies to save and get filter and set Filter in mobile view

--- StreetbeatzLB_Frontend/src/app/core/services/smfCookieService.ts
+++ StreetbeatzLB_Frontend/src/app/core/services/smfCookieService.ts
@@ -8,15 +8,48 @@ import {Filter} from "../models/filter.model";
 })
 export class SmfCookieService {
 
-  constructor(private filterCookieService:CookieService) {
+  constructor(private smfService: CookieService) {
   }
 
   setFilterCookies(filter: Filter) {
-    alert(<string>filter.date?.toDateString())
-    this.filterCookieService.set("filterCookieDate",<string>filter.date?.toISOString())
-    this.filterCookieService.set("filterCookieHours",filter.time?.hours+"")
-    this.filterCookieService.set("filterCookieMinutes",filter.time?.minutes+"")
-    this.filterCookieService.set("filterCookieMinutes",filter.time?.minutes+"")
-    alert(this.filterCookieService.get("filterCookieDate"))
+    console.log("Write Filter as Cookies")
+
+    if (filter.date) {
+      this.smfService.set("filterCookieDate", <string>filter.date.toISOString())
+    } else {
+      this.smfService.delete("filterCookieDate")
+    }
+    if (filter.time) {
+      this.smfService.set("filterCookieHours", filter.time.hours + "")
+      this.smfService.set("filterCookieMinutes", filter.time.minutes + "")
+    }else {
+      this.smfService.delete("filterCookieHours")
+      this.smfService.delete("filterCookieMinutes")
+    }
+    if (filter.artist) {
+      this.smfService.set("filterCookieArtist", <string>filter.artist.toString())
+    }else {
+      this.smfService.delete("filterCookieArtist")
+    }
+    if (filter.stage) {
+      this.smfService.set("filterCookieStage", <string>filter.stage.toString())
+    }else {
+      this.smfService.delete("filterCookieStage")
+    }
+  }
+
+  getFilterCookies(): Filter {
+    console.log("Get Filter from Cookies")
+
+    return new Filter(
+      this.smfService.check("filterCookieDate") ? new Date(<string>this.smfService.get("filterCookieDate")) : null,
+      (this.smfService.check("filterCookieHours") &&
+        this.smfService.check("filterCookieMinutes")) ? {
+        hours: Number(this.smfService.get("filterCookieHours")),
+        minutes: Number(this.smfService.get("filterCookieMinutes"))
+      } : null,
+      this.smfService.check("filterCookieArtist") ? this.smfService.get("filterCookieArtist") : null,
+      this.smfService.check("filterCookieStage") ? this.smfService.get("filterCookieStage") : null,
+    );
   }
 }
--- StreetbeatzLB_Frontend/src/app/shared/components/filter/filter-mobile/filter-mobile.component.ts
+++ StreetbeatzLB_Frontend/src/app/shared/components/filter/filter-mobile/filter-mobile.component.ts
@@ -12,13 +12,29 @@ export class FilterMobileComponent implements OnInit {
   panelOpenState = false;
   filter: Filter = new Filter(null, null, null, null);
   filterTypes: string[] = []; // 'Date', 'Artist', 'Stage'
-  @Input() inFilter!: Filter;
+  @Input() inFilter!: Filter; //Was macht dieser inFilter?
   @Output() outFilter = new EventEmitter<Filter>();
-  constructor(private smfCookieService:SmfCookieService) {
+
+  constructor(private smfCookieService: SmfCookieService) {
   }
+
   ngOnInit() {
-    if (this.inFilter) {
-      this.filter = this.inFilter;
+    this.filter = this.smfCookieService.getFilterCookies();
+    this.updateFilterTypes()
+  }
+
+  private updateFilterTypes() {
+    if (this.filter.date || this.filter.date != undefined) {
+      this.add("Date")
+    }
+    if (this.filter.time || this.filter.time != undefined) {
+      this.add("Time")
+    }
+    if (this.filter.artist || this.filter.artist != undefined) {
+      this.add("Artist")
+    }
+    if (this.filter.stage || this.filter.stage != undefined) {
+      this.add("Stage")
     }
   }
 
@@ -28,7 +44,6 @@ export class FilterMobileComponent implements OnInit {
     switch (filterType) {
       case 'Date':
         this.filter.date = null;
-        this.remove('Time');
         break;
       case 'Time':
         this.filter.time = null;
@@ -41,6 +56,7 @@ export class FilterMobileComponent implements OnInit {
         break;
     }
 
+    this.smfCookieService.setFilterCookies(this.filter);
     if (index >= 0) {
       this.filterTypes.splice(index, 1);
     }
