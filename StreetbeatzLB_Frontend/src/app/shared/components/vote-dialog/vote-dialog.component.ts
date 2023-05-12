7f0ba05b3918cd6f70844d907faa6c9b0cb457c2
Michel Jouaux <micheljouaux@gmx.de>
2023-05-12 02:17:29 +0200

    Add: if-statment to reuse the dialog when someone has already voted

--- StreetbeatzLB_Frontend/src/app/shared/components/vote-dialog/vote-dialog.component.ts
+++ StreetbeatzLB_Frontend/src/app/shared/components/vote-dialog/vote-dialog.component.ts
@@ -11,11 +11,17 @@ export class VoteDialogComponent {
   yesButton: string = "Yes";
   noButton: string = "No";
   voteTitle: string = "Vote";
-
   text: string = "Are you sure that you want to vote for this artist?";
 
+
   constructor(public dialogRef: MatDialogRef<VoteDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: DialogData,){
+    if (this.data.hasCookie){
+      this.text = "You have already voted for this artist:";
+    }else {
       this.data.hasChosen = true;
+    }
+    console.log("Dialog: ");
+    console.log(this.data);
   }
 
   onNoClick(): void{
