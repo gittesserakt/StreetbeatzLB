d2d099978cfffd00a57f111f6ac06e4d88ee0443
Michel Jouaux <micheljouaux@gmx.de>
2023-05-17 01:57:34 +0200

    Fix: Somehow forgot to commit app.component.ts

--- StreetbeatzLB_Frontend/src/app/app.component.ts
+++ StreetbeatzLB_Frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, ViewChild, AfterViewInit, OnInit} from '@angular/core';
+import {Component, ViewChild, AfterViewInit, OnInit, HostListener} from '@angular/core';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import {AuthService} from "@auth0/auth0-angular";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,12 @@ export class AppComponent implements AfterViewInit, OnInit {
   title = 'StreetbeatzLB_Frontend';
   @ViewChild(NavbarComponent) navbar!: NavbarComponent;
   navbarHeight: number = 64;
+  device:String = "Web";
+  displayMap = new Map([
+    [Breakpoints.Handset, 'Handset'],
+    [Breakpoints.Tablet, 'Tablet'],
+    [Breakpoints.WebLandscape, 'Web']
+  ])
 
   ngAfterViewInit(){
     this.navbarHeight = this.navbar.navbarHeight;
@@ -21,5 +28,26 @@ export class AppComponent implements AfterViewInit, OnInit {
   }
 
   isAuth0Loading$ = this.authService.isLoading$;
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,private breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([
+      Breakpoints.Handset,
+      Breakpoints.Tablet,
+      Breakpoints.WebLandscape
+    ]).subscribe(result =>{
+      //console.log(result);
+      for(const query of Object.keys(result.breakpoints)){
+        if(result.breakpoints[query]){
+          this.device = this.displayMap.get(query) as String;
+        }
+      }
+      //console.log(this.device);
+    })
+  }
+
+  isSticky: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    this.isSticky = window.scrollY >= 250;
+  }
 }
