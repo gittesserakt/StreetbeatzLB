a0f4b11bc6c3e39fef247002906a64b4d85fdad9
Michel Jouaux <micheljouaux@gmx.de>
2023-05-19 17:26:59 +0200

    Add: Created landingpage-sponsor component

---
+++ StreetbeatzLB_Frontend/src/app/shared/components/landingpage-sponsor/landingpage-sponsor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landingpage-sponsor',
+  templateUrl: './landingpage-sponsor.component.html',
+  styleUrls: ['./landingpage-sponsor.component.scss']
+})
+export class LandingpageSponsorComponent {
+  height: number = 667; //nope
+
+}
