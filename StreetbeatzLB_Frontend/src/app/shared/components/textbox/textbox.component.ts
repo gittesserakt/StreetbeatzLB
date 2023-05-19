635f14497d7b787c5a142c8a5bd9c7387b3e7586
Michel Jouaux <micheljouaux@gmx.de>
2023-05-19 17:23:01 +0200

    Fix: Text is now variable

--- StreetbeatzLB_Frontend/src/app/shared/components/textbox/textbox.component.ts
+++ StreetbeatzLB_Frontend/src/app/shared/components/textbox/textbox.component.ts
@@ -8,6 +8,5 @@ import {Component, Input} from '@angular/core';
 export class TextboxComponent {
   @Input() screenHeightPX!:number;
   @Input() screenWidthPX!:number;
-  text: string = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
-
+  @Input() text!:string;
 }
