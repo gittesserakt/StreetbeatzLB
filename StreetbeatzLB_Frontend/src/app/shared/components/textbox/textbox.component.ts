b334ec26e63fa70c58a0b24a7810323e9b404ee2
Michel Jouaux <micheljouaux@gmx.de>
2023-05-19 10:53:38 +0200

    Add: Created textbox component

---
+++ StreetbeatzLB_Frontend/src/app/shared/components/textbox/textbox.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-textbox',
+  templateUrl: './textbox.component.html',
+  styleUrls: ['./textbox.component.scss']
+})
+export class TextboxComponent {
+  @Input() screenHeightPX!:number;
+  @Input() screenWidthPX!:number;
+  text: string = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+
+}
