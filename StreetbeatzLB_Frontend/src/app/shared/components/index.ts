9a515deb64832d828b456c543d5e319b829caac8
Dennis <dennisdeifel@gmail.com>
2023-05-19 19:39:38 +0200

    date-time-picker component generated

--- StreetbeatzLB_Frontend/src/app/shared/components/index.ts
+++ StreetbeatzLB_Frontend/src/app/shared/components/index.ts
@@ -1,47 +1,42 @@
-import {NavbarComponent} from "./navbar/navbar.component";
-import {FooterComponent} from "./footer/footer.component";
-import {ImageSliderComponent} from "./image-slider/image-slider.component";
-import {PerformanceTileComponent} from "./performance-tile/performance-tile.component";
-import {NavbarMobileComponent} from "./navbar/navbar-mobile/navbar-mobile.component";
-import {NavbarDesktopComponent} from "./navbar/navbar-desktop/navbar-desktop.component";
-import {FilterComponent} from "./filter/filter.component";
-import {FilterMobileComponent} from "./filter/filter-mobile/filter-mobile.component";
-import {FilterDesktopComponent} from "./filter/filter-desktop/filter-desktop.component";
-import {FilterDatepickerComponent} from "./filter/filter-datepicker/filter-datepicker.component";
-import {FilterArtistselectorComponent} from "./filter/filter-artistselector/filter-artistselector.component";
-import {FilterStageselectorComponent} from "./filter/filter-stageselector/filter-stageselector.component";
-import {PerformancePopupComponent} from "./performance-popup/performance-popup.component";
-import {LoginButtonComponent} from "./login-button/login-button.component";
-import {LogoutButtonComponent} from "./logout-button/logout-button.component";
-import {PageLoaderComponent} from "./page-loader/page-loader.component";
-import {FilterTimepickerComponent} from "./filter/filter-timepicker/filter-timepicker.component";
-import {PerformanceViewComponent} from "./performance-view/performance-view.component";
-
+import { DateTimePickerComponent } from "./date-time-picker/date-time-picker.component";
+import { FilterArtistselectorComponent } from "./filter/filter-artistselector/filter-artistselector.component";
+import { FilterComponent } from "./filter/filter.component";
+import { FilterDatepickerComponent } from "./filter/filter-datepicker/filter-datepicker.component";
+import { FilterDesktopComponent } from "./filter/filter-desktop/filter-desktop.component";
+import { FilterMobileComponent } from "./filter/filter-mobile/filter-mobile.component";
+import { FilterStageselectorComponent } from "./filter/filter-stageselector/filter-stageselector.component";
+import { FilterTimepickerComponent } from "./filter/filter-timepicker/filter-timepicker.component";
+import { FooterComponent } from "./footer/footer.component";
+import { ImageSliderComponent } from "./image-slider/image-slider.component";
+import { LoginButtonComponent } from "./login-button/login-button.component";
+import { LogoutButtonComponent } from "./logout-button/logout-button.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { NavbarDesktopComponent } from "./navbar/navbar-desktop/navbar-desktop.component";
+import { NavbarMobileComponent } from "./navbar/navbar-mobile/navbar-mobile.component";
+import { PageLoaderComponent } from "./page-loader/page-loader.component";
+import { PerformancePopupComponent } from "./performance-popup/performance-popup.component";
+import { PerformanceTileComponent } from "./performance-tile/performance-tile.component";
+import { PerformanceViewComponent } from "./performance-view/performance-view.component";
 
 export const COMPONENTS = [
-  NavbarComponent,
+  DateTimePickerComponent,
+  FilterArtistselectorComponent,
+  FilterComponent,
+  FilterDatepickerComponent,
+  FilterDesktopComponent,
+  FilterMobileComponent,
+  FilterStageselectorComponent,
+  FilterTimepickerComponent,
   FooterComponent,
   ImageSliderComponent,
-  PerformanceTileComponent,
-  PerformancePopupComponent,
-  NavbarMobileComponent,
+  LoginButtonComponent,
+  LogoutButtonComponent,
+  NavbarComponent,
   NavbarDesktopComponent,
-  FilterComponent,
-  FilterMobileComponent,
-  FilterDesktopComponent,
-  FilterDatepickerComponent,
-  FilterArtistselectorComponent,
-  FilterStageselectorComponent,
-  LoginButtonComponent,
-  LogoutButtonComponent,
+  NavbarMobileComponent,
   PageLoaderComponent,
-  ImageSliderComponent,
-  LoginButtonComponent,
-  LogoutButtonComponent,
-  PageLoaderComponent,
-  FilterTimepickerComponent,
-  PageLoaderComponent,
-  ImageSliderComponent,
-  PerformanceViewComponent,
+  PerformancePopupComponent,
+  PerformanceTileComponent,
+  PerformanceViewComponent
 ]
 
