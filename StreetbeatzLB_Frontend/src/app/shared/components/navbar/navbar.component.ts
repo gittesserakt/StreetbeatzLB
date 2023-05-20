2f97916dbebb6a7c54e4055306d2c2e3934166a8
Michel Jouaux <micheljouaux@gmx.de>
2023-05-20 18:34:40 +0200

    Fix: Desktop navbar is now transparent
    There is still a bug/problem because I'm unable to change the color of the link text.

--- StreetbeatzLB_Frontend/src/app/shared/components/navbar/navbar.component.ts
+++ StreetbeatzLB_Frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -9,14 +9,9 @@ import {AuthService} from "@auth0/auth0-angular";
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  @Input() navbarHeight: number = 64;
   isAuthenticated$ = this.authService.isAuthenticated$;
 
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private authService: AuthService) {
     iconRegistry.addSvgIcon('streetbeatz', sanitizer.bypassSecurityTrustResourceUrl('./assets/streetbeatzLogo/logo.svg'));
-  }
-
-  ngOnInit(): void {
-    document.documentElement.style.setProperty('--navbar-height', this.navbarHeight + 'px');
   }
 }
