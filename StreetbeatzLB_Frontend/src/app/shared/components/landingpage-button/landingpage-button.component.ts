962a4dc1c9f181b6b75577322cf6303dc3197bc1
Michel Jouaux <micheljouaux@gmx.de>
2023-05-20 23:24:39 +0200

    Add: Added the button as well as text, icons and routing to the pages

--- StreetbeatzLB_Frontend/src/app/shared/components/landingpage-button/landingpage-button.component.ts
+++ StreetbeatzLB_Frontend/src/app/shared/components/landingpage-button/landingpage-button.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-landingpage-button',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class LandingpageButtonComponent {
 
+  constructor(private router: Router) {
+  }
+  performanceButtonClick(){
+    this.router.navigate(['/performances']);
+  }
+  voteButtonClick(){
+    this.router.navigate(['/vote']);
+  }
+  mapButtonClick(){
+    this.router.navigate(['/map']);
+  }
 }
