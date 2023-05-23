0202a363fcf9db122db1edb188aeb5969c831e82
Hannes Jetter <hannes.jetter@googlemail.com>
2023-05-23 08:17:08 +0200

    various bug fixes

--- StreetbeatzLB_Frontend/src/app/shared/components/sidenav/sidenav.component.ts
+++ StreetbeatzLB_Frontend/src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,7 +1,8 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Inject, Output} from '@angular/core';
 import {MatIconRegistry} from "@angular/material/icon";
 import {DomSanitizer} from "@angular/platform-browser";
 import {AuthService} from "@auth0/auth0-angular";
+import {APP_BASE_HREF} from "@angular/common";
 
 @Component({
   selector: 'app-sidenav',
@@ -10,10 +11,14 @@ import {AuthService} from "@auth0/auth0-angular";
 })
 export class SidenavComponent {
   isAuthenticated$ = this.authService.isAuthenticated$;
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private authService: AuthService) {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private authService: AuthService,
+              @Inject(APP_BASE_HREF) public baseHref: string) {
     iconRegistry.addSvgIcon('streetbeatz', sanitizer.bypassSecurityTrustResourceUrl('./assets/streetbeatzLogo/logo.svg'));
   }
   @Output() toggleSidenav = new EventEmitter();
+  sidenavBackground: any = {
+    'background-image': `url(${this.baseHref + 'assets/design/landingpage_blue-magenta-background.svg'})`,
+  };
 
   toggleSidenavEvent() {
     this.toggleSidenav.emit();
