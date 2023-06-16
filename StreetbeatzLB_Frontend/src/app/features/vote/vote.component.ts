a367510142a22d990375fa668bff715f37130891
Michel Jouaux <micheljouaux@gmx.de>
2023-06-16 02:09:42 +0200

    Fix: Removed some unnecessary lines and commented some debug console.log

--- StreetbeatzLB_Frontend/src/app/features/vote/vote.component.ts
+++ StreetbeatzLB_Frontend/src/app/features/vote/vote.component.ts
@@ -26,16 +26,17 @@ export interface DialogData {
 })
 export class VoteComponent implements OnInit {
   @ViewChildren('checkbox') checkbox!: QueryList<MatCheckbox>;
+  selectedCount: number = 0;
+  artists?: Artist[];
 
+  //region displayVariables
   voteButton: string = 'Vote';
   remainingVotes: number = 2;
   voteTitle: string = "Please select up to 2 artist you want to vote for:";
+  //endregion
 
-  screenHeightPX: number = 0;
-  centerList?: number;
 
-  selectedCount: number = 0;
-
+  //region dialogDateInterface
   chosenArtist1?: Artist;
   chosenArtist2?: Artist;
   voteCount: number = 0;
@@ -43,32 +44,19 @@ export class VoteComponent implements OnInit {
   hasCookie: boolean = false;
   showThanks: boolean = true;
 
-  artists?: Artist[];
-
-  device?: string;
-  displayMap = new Map([
-    [Breakpoints.HandsetPortrait, 'HandsetPortrait'], //max-width: 599.98
-    [Breakpoints.HandsetLandscape, 'HandsetLandscape'], //max-width: 959.98px
-    [Breakpoints.TabletPortrait, 'TabletPortrait'], //min-width: 600px & max-width: 839.98px
-    [Breakpoints.TabletLandscape, 'TabletLandscape'], //min-width: 960px & max-width: 1279.98px
-    [Breakpoints.WebPortrait, 'WebPortrait'], //min-width: 840px
-    [Breakpoints.WebLandscape, 'WebLandscape'] //min-width: 1280px
-  ]);
+  //endregion
 
   constructor(public dialog: MatDialog, private artistService: ArtistService, private voteService: VoteService,
-              private breakpointObserver: BreakpointObserver, private smfCookieService: SmfCookieService, private _snackbar: MatSnackBar) {
-    // this.onResize();
-  }
+              private smfCookieService: SmfCookieService, private _snackbar: MatSnackBar) {}
 
   ngOnInit(): void {
-    // this.onResize();
     this.initCheckBoxList();
   }
 
   initCheckBoxList(): void {
-    console.log("check cache");
-    if(this.smfCookieService.getVoteCookies()[0] != "" && this.smfCookieService.getVoteCookies()[1] != ""){
-      console.log("two votes found");
+    // console.log("check cache");
+    if (this.smfCookieService.getVoteCookies()[0] != "" && this.smfCookieService.getVoteCookies()[1] != "") {
+      // console.log("two votes found");
 
       this.getArtistByNamePromise(this.smfCookieService.getVoteCookies()[0]).then((artist1) => {
         this.chosenArtist1 = artist1 as Artist;
@@ -79,14 +67,13 @@ export class VoteComponent implements OnInit {
           console.log(this.chosenArtist2);
 
           this.getAllArtists();
-          console.log('List two');
 
           setTimeout(() => {
             this.remainingVotes = 0;
             this.selectedCount = 2;
             this.hasVoted = true;
             this.hasCookie = true;
-            console.log("open dialog");
+            // console.log("open dialog");
             this.openDialog();
           }, 100);
 
@@ -94,56 +81,35 @@ export class VoteComponent implements OnInit {
             this.checkAndDisableCheckbox(this.chosenArtist1?.name);
             this.checkAndDisableCheckbox(this.chosenArtist2?.name);
           }, 100);
-        }).catch((error) =>{
+        }).catch((error) => {
           console.log(error);
         });
-      }).catch((error) =>{
+      }).catch((error) => {
         console.log(error);
       });
-    }
-    else if(this.smfCookieService.getVoteCookies()[0] != ""){
-      console.log("one vote found");
+    } else if (this.smfCookieService.getVoteCookies()[0] != "") {
+      // console.log("one vote found");
 
       this.getArtistByNamePromise(this.smfCookieService.getVoteCookies()[0]).then((artist) => {
         this.chosenArtist1 = artist as Artist;
-        console.log('Cookie 1', this.chosenArtist1);
 
         this.remainingVotes = 1;
         this.selectedCount = 1;
 
         this.getAllArtists();
-        console.log('List one');
 
         setTimeout(() => {
-          console.log('no more timeout');
           this.checkAndDisableCheckbox(this.chosenArtist1?.name);
         }, 100);
-      }).catch((error) =>{
+      }).catch((error) => {
         console.log(error);
       });
-    }
-    else{
-      this.getAllArtists();
-      console.log('List alone');
-    }
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event?: any) {
-    this.getBreakpoint();
-    this.screenHeightPX = window.innerHeight;
-    if (this.device == 'HandsetPortrait') {
-      this.centerList = 100;
-    } else if (this.device == 'WebLandscape') {
-      this.centerList = 44.5;
     } else {
-      this.centerList = 50;
+      this.getAllArtists();
     }
   }
 
   onClick() {
-    console.log(this.chosenArtist1);
-    console.log(this.chosenArtist2);
     if (this.chosenArtist1 != undefined) {
       this.openDialog();
     } else {
@@ -156,11 +122,10 @@ export class VoteComponent implements OnInit {
   getAllArtists(): void {
     this.artistService.getAllArtists().subscribe((response) => {
       const {data, error} = response;
-      console.log('artist', response);
 
       if (data) {
         this.artists = data as Artist[];
-        console.log(this.artists);
+        // console.log(this.artists);
       }
       if (error) {
         console.log(error);
@@ -168,12 +133,12 @@ export class VoteComponent implements OnInit {
     })
   }
 
-  getArtistByNamePromise(name: string): Promise<any>{
+  getArtistByNamePromise(name: string): Promise<any> {
     return new Promise((resolve, reject) => {
       this.artistService.getArtistByName(name).subscribe((response) => {
         const {data, error} = response;
         if (data) {
-          console.log(data);
+          // console.log(data);
           resolve(data);
         }
         if (error) {
@@ -184,41 +149,19 @@ export class VoteComponent implements OnInit {
   }
 
   voteForArtist(): void {
-    if(this.smfCookieService.getVoteCookies()[0] == '' && this.smfCookieService.getVoteCookies()[1] == ""){
+    if (this.smfCookieService.getVoteCookies()[0] == '' && this.smfCookieService.getVoteCookies()[1] == "") {
       if (this.chosenArtist1 && this.chosenArtist2 != undefined) {
-        console.log('vote for two');
         this.voteService.voteForArtist(this.chosenArtist1.name);
         this.voteService.voteForArtist(this.chosenArtist2.name);
         this.smfCookieService.setVoteCookies(this.chosenArtist1, this.chosenArtist2, this.voteCount, true);
       } else if (this.chosenArtist1 != undefined && this.chosenArtist2 == undefined) {
-        console.log('vote for first');
         this.voteService.voteForArtist(this.chosenArtist1.name);
         this.smfCookieService.setVoteCookies(this.chosenArtist1, undefined, this.voteCount, false);
       }
-    }else {
-      console.log('vote for second');
+    } else {
       this.voteService.voteForArtist(this.chosenArtist2!.name);
       this.smfCookieService.setVoteCookies(this.chosenArtist1, this.chosenArtist2, this.voteCount, true);
     }
-  }
-
-  getBreakpoint() {
-    this.breakpointObserver.observe([
-      Breakpoints.HandsetPortrait,
-      Breakpoints.HandsetLandscape,
-      Breakpoints.TabletPortrait,
-      Breakpoints.TabletLandscape,
-      Breakpoints.WebPortrait,
-      Breakpoints.WebLandscape
-    ]).subscribe(result => {
-      for (const query of Object.keys(result.breakpoints)) {
-        if (result.breakpoints[query]) {
-          this.device = this.displayMap.get(query) as string;
-        }
-      }
-    })
-
-    console.log('Breakpoint: ' + this.device)
   }
 
   openDialog(): void {
@@ -236,7 +179,6 @@ export class VoteComponent implements OnInit {
     if (!this.hasCookie && this.showThanks) {
       dialogRef.afterClosed().subscribe(result => {
         if (result != undefined) {
-          // this.hasChosen = result;
           this.voteCount = result;
           this.remainingVotes -= this.voteCount;
           this.voteForArtist();
@@ -267,11 +209,8 @@ export class VoteComponent implements OnInit {
         if (this.selectedCount == 1) {
           this.artistService.getArtistByName(event.source.value).subscribe((response) => {
             const {data, error} = response
-            console.log('artist', response);
             if (data) {
               this.chosenArtist1 = data as Artist;
-              console.log('1: ', data as Artist);
-              console.log('1.1: ', this.chosenArtist1.name);
             }
             if (error) {
               console.log(error);
@@ -284,8 +223,6 @@ export class VoteComponent implements OnInit {
             console.log('artist', response);
             if (data) {
               this.chosenArtist2 = data as Artist;
-              console.log('2: ', data as Artist);
-              console.log('2.1: ', this.chosenArtist2.name);
             }
             if (error) {
               console.log(error);
@@ -296,11 +233,11 @@ export class VoteComponent implements OnInit {
     } else {
       this.selectedCount--;
       if (this.selectedCount == 1) {
-        if(event.source.value == this.chosenArtist1?.name){
+        if (event.source.value == this.chosenArtist1?.name) {
           this.chosenArtist1 = this.chosenArtist2;
           this.chosenArtist2 = undefined;
         }
-        if(event.source.value == this.chosenArtist2?.name){
+        if (event.source.value == this.chosenArtist2?.name) {
           this.chosenArtist2 = undefined;
         }
       }
@@ -311,10 +248,10 @@ export class VoteComponent implements OnInit {
     }
   }
 
-  checkAndDisableCheckbox(name: string | undefined): void{
-    console.log('Check Disable', name);
+  checkAndDisableCheckbox(name: string | undefined): void {
+    // console.log('Check Disable', name);
     this.checkbox.forEach(box => {
-      if(box.value === name){
+      if (box.value === name) {
         box.checked = true;
         box.disabled = true;
       }
