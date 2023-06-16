2839d68fc0879d5beda3d096329edd13ab3b0b72
Hannes Jetter <hannes.jetter@googlemail.com>
2023-06-17 01:24:03 +0200

    trying single line string instead of multiline

--- Deployment/Production/builder-sb/build_project.sh
+++ Deployment/Production/builder-sb/build_project.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 # replace username for mariadb with the one in the .env file
 echo "Replacing username for mariadb"
@@ -19,13 +20,7 @@ if [ "$USE_PROXY" = "true" ]; then
 
   # if PUBLIC_DB_ACCESS is set to true, replace environment variables in nginx stream config
   if [ "$PUBLIC_DB_ACCESS" = "true" ]; then
-    # multiline string for nginx stream config
-    stream_config="stream {
-      server {
-        listen 3306;
-        proxy_pass $SERVER_LAN_IP:$DB_PORT;
-      }
-    }"
+    stream_config="stream { server { listen 3306; proxy_pass $SERVER_LAN_IP:$DB_PORT; } }"
 
     #insert multiline stream config string into nginx.conf at line 12
     sed -i "12i $stream_config" /data/Deployment/ReverseProxy/nginx-main.conf
