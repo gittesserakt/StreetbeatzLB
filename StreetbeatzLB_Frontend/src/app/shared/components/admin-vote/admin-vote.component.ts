04022915569e63c4c3805a29b6e4ec14f1b97825
Michel Jouaux <micheljouaux@gmx.de>
2023-06-21 20:59:41 +0200

    Add: Admin-vote.component is done only the dialog for vote is missing

--- StreetbeatzLB_Frontend/src/app/shared/components/admin-vote/admin-vote.component.ts
+++ StreetbeatzLB_Frontend/src/app/shared/components/admin-vote/admin-vote.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Artist} from "../../../core/models/artist.model";
 import {ArtistService} from "../../../core/services/artist.service";
 import {VoteService} from "../../../core/services/vote.service";
@@ -8,25 +8,37 @@ import {VoteService} from "../../../core/services/vote.service";
   templateUrl: './admin-vote.component.html',
   styleUrls: ['./admin-vote.component.scss']
 })
-export class AdminVoteComponent {
-  isClosed: boolean = false;
-  slideToggleText:string ="Close vote!";
+export class AdminVoteComponent implements OnInit{
+  voteStatus: boolean = true;
+  slideToggleText:string ="Vote is open!";
   artists?: Artist[];
   topArtists?: Artist[];
 
   constructor( private artistService: ArtistService, private voteService: VoteService) {
     this.getAllArtist();
+    this.getVoteStatus();
+  }
+
+  ngOnInit(): void {
+    if(!this.voteStatus){
+      this.showWinner();
+      this.slideToggleText = "Vote is closed!";
+    }
   }
 
   onToggleClick():void{
-    this.isClosed = !this.isClosed;
-    if(this.isClosed){
-      this.voteService.closeVoting();
-      this.topArtists = this.artists;
-      this.topArtists?.sort(({vote_count:a},{vote_count:b}) => b - a);
+    if(!this.voteStatus){
+      this.voteService.openVoting().then(() =>{
+        this.topArtists = undefined;
+        this.voteStatus = true;
+        this.slideToggleText ="Vote is open!";
+      });
     }else{
-      this.voteService.openVoting();
-      this.topArtists = undefined;
+      this.voteService.closeVoting().then(() => {
+        this.showWinner();
+        this.voteStatus = false;
+        this.slideToggleText = "Vote is closed!";
+      });
     }
   }
 
@@ -43,5 +55,25 @@ export class AdminVoteComponent {
         console.log(error);
       }
     });
+  }
+
+  getVoteStatus(){
+    this.voteService.getVoteStatus().subscribe((response) => {
+      const {data, error} = response;
+      console.log(response);
+
+      if (data) {
+        this.voteStatus = data as boolean;
+        // console.log("Vote Status:" + this.voteStatus);
+      }
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+
+  showWinner(){
+    this.topArtists = this.artists;
+    this.topArtists?.sort(({vote_count:a},{vote_count:b}) => b - a);
   }
 }
